0c5d04df174de3ee655d1d8e4210cc987d6be1a5
Karl Fristedt <karl.fristedt@authority.se>
2019-10-11 15:50:03 +0200

    Added RequestFactory class and Interface

---
+++ src/PayEx.Checkout.Episerver/IRequestFactory.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.Commerce.Order;
+using Mediachase.Commerce;
+using Mediachase.Commerce.Orders.Dto;
+using SwedbankPay.Client;
+using SwedbankPay.Client.Models.Request;
+
+namespace PayEx.Checkout.Episerver
+{
+    public interface IRequestFactory
+    {
+        PaymentOrderRequestContainer Create(IOrderGroup orderGroup, IMarket market, PaymentMethodDto paymentMethodDto, string consumerProfileRef = null);
+    }
+}
