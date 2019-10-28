b41c25552d32859607d7ee5a8387f9931192fefd
Karl Fristedt <karl.fristedt@authority.se>
2019-10-28 15:15:56 +0100

    changed method name

--- demo/Sources/EPiServer.Reference.Commerce.Shared/CustomRequestFactory.cs
+++ demo/Sources/EPiServer.Reference.Commerce.Shared/CustomRequestFactory.cs
@@ -1,21 +1,22 @@
-﻿using EPiServer.Commerce.Order;
+﻿using System.Linq;
+using EPiServer.Commerce.Order;
 using Mediachase.Commerce;
+using Mediachase.Commerce.Catalog;
 using Mediachase.Commerce.Orders.Dto;
-using System.Linq;
-using SwedbankPay.Checkout.Episerver;
 using SwedbankPay.Checkout.Episerver.Common;
-using SwedbankPay.Sdk.Models.Request;
+using SwedbankPay.Sdk.PaymentOrders;
+
 
 namespace EPiServer.Reference.Commerce.Shared
 {
     public class CustomRequestFactory : RequestFactory
     {
-        public CustomRequestFactory(ICheckoutConfigurationLoader checkoutConfigurationLoader, IOrderGroupCalculator orderGroupCalculator, IShippingCalculator shippingCalculator) : base(checkoutConfigurationLoader, orderGroupCalculator, shippingCalculator)
+        public CustomRequestFactory(ICheckoutConfigurationLoader checkoutConfigurationLoader, IOrderGroupCalculator orderGroupCalculator, IShippingCalculator shippingCalculator, ITaxCalculator taxCalculator, ReferenceConverter referenceConverter, IContentRepository contentRepository) : base(checkoutConfigurationLoader, orderGroupCalculator, shippingCalculator, taxCalculator, referenceConverter, contentRepository)
         {
         }
-        public override PaymentOrderRequestContainer CreatePaymentOrderRequestContainer(IOrderGroup orderGroup, IMarket market, PaymentMethodDto paymentMethodDto, string consumerProfileRef = null)
+        public override PaymentOrderRequestContainer GetPaymentOrderRequestContainer(IOrderGroup orderGroup, IMarket market, PaymentMethodDto paymentMethodDto, string consumerProfileRef = null)
         {
-            var test = base.CreatePaymentOrderRequestContainer(orderGroup, market, paymentMethodDto, consumerProfileRef);
+            var test = base.GetPaymentOrderRequestContainer(orderGroup, market, paymentMethodDto, consumerProfileRef);
             test.Paymentorder.Amount = 500;
             test.Paymentorder.VatAmount = 0;
             
