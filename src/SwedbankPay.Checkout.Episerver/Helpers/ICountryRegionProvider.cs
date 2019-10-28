178a424f46e1dce1d2a6d7022b92a3f1fbe791d6
Karl Fristedt <karl.fristedt@authority.se>
2019-10-28 15:13:13 +0100

    Swedbank client upgrade to 0.1.1-alpha

--- src/SwedbankPay.Checkout.Episerver/Helpers/ICountryRegionProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SwedbankPay.Checkout.Episerver.Helpers
-{
-    public interface ICountryRegionProvider
-    {
-        string GetStateName(string twoLetterCountryCode, string stateCode);
-        string GetStateCode(string twoLetterCountryCode, string stateName);
-    }
-}
