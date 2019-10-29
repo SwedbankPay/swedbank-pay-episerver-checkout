4baac5965168f069ed9636c7cf7992b67bb12c38
Daniel Sundqvist <daniel.sundqvist@authority.se>
2019-10-29 15:54:37 +0100

    Added SiteInitialization to Manager to Intercept TaxCalculator

---
+++ demo/Sources/EPiServer.Reference.Commerce.Manager/SiteInitialization.cs
@@ -0,0 +1,33 @@
+﻿namespace EPiServer.Reference.Commerce.Manager
+{
+    using EPiServer.Commerce.Order;
+    using EPiServer.Framework;
+    using EPiServer.Framework.Initialization;
+    using EPiServer.ServiceLocation;
+
+    using Mediachase.Commerce.Catalog;
+
+    using SwedbankPay.Checkout.Episerver.Common;
+
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class SiteInitialization : IConfigurableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+
+        }
+
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            var services = context.Services;
+            services.Intercept<ITaxCalculator>((locator, calculator) =>
+                new SwedbankPayTaxCalculator(locator.GetInstance<IContentRepository>(),
+                    locator.GetInstance<ReferenceConverter>()));
+        }
+    }
+}
