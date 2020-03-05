43ee6ea3338072598d03f4d7e315c285136fb8d3
Karl Fristedt <karl.fristedt@authority.se>
2020-03-05 09:40:15 +0100

    Removed ILogger references and Microsoft.Extensions.Logging.Abstractions

--- src/SwedbankPay.Episerver.Checkout.Common/ISwedbankPayClientFactory.cs
+++ src/SwedbankPay.Episerver.Checkout.Common/ISwedbankPayClientFactory.cs
@@ -1,14 +1,14 @@
 ﻿using Mediachase.Commerce;
 using Mediachase.Commerce.Orders.Dto;
-using Microsoft.Extensions.Logging;
+
 using SwedbankPay.Sdk;
 
 namespace SwedbankPay.Episerver.Checkout.Common
 {
     public interface ISwedbankPayClientFactory
     {
-        ISwedbankPayClient Create(IMarket market, ILogger logger = null);
-        ISwedbankPayClient Create(PaymentMethodDto paymentMethodDto, MarketId marketMarketId, ILogger logger = null);
-        ISwedbankPayClient Create(ConnectionConfiguration connectionConfiguration, ILogger logger = null);
+        ISwedbankPayClient Create(IMarket market);
+        ISwedbankPayClient Create(PaymentMethodDto paymentMethodDto, MarketId marketMarketId);
+        ISwedbankPayClient Create(ConnectionConfiguration connectionConfiguration);
     }
 }
