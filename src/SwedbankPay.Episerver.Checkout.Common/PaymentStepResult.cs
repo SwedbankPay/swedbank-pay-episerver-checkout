b30c8a0f893f810c724d90f7d8835c90f28d542d
Daniel Sundqvist <daniel.sundqvist@authority.se>
2020-04-07 11:28:00 +0200

    Added PaymentStepResult class

---
+++ src/SwedbankPay.Episerver.Checkout.Common/PaymentStepResult.cs
@@ -0,0 +1,8 @@
+﻿namespace SwedbankPay.Episerver.Checkout.Common
+{
+    public class PaymentStepResult
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
