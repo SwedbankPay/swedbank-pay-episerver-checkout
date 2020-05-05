06e7feadc55369f22dd30fcf5af028de1296ed57
Daniel Sundqvist <daniel.sundqvist@authority.se>
2020-05-05 15:53:51 +0200

    Get PurchaseOrder by PayeeReference

--- src/SwedbankPay.Episerver.Checkout.Common/ISwedbankPayService.cs
+++ src/SwedbankPay.Episerver.Checkout.Common/ISwedbankPayService.cs
@@ -5,5 +5,6 @@ namespace SwedbankPay.Episerver.Checkout.Common
     public interface ISwedbankPayService
     {
         IPurchaseOrder GetPurchaseOrderBySwedbankPayOrderId(string orderId);
+        IPurchaseOrder GetByPayeeReference(string payeeReference);
     }
 }
